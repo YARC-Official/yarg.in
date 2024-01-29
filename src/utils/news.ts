b9afb56a20200221c9b6e7747bb83349b109dac4
Pantotone <guustavopantoja@gmail.com>
2024-01-29 17:53:16 -0300

    feat: create article schema

--- src/utils/news.ts
+++ src/utils/news.ts
@@ -1,8 +1,47 @@
 import { TagColor } from "@/components/Tag"
 import { NEWS_SOURCE } from "./constants"
+import { z } from "zod";
+
+export const ValidArticleTypes = z.union([
+    z.literal("update"), 
+    z.literal("announcement"), 
+    z.literal("fix"), 
+    z.literal("event"), 
+    z.literal("tournament"), 
+    z.literal("dlc"),
+]);
+
+export type ArticleTypes = z.infer<typeof ValidArticleTypes>;
+
+export const ArticleAuthor = z.object({
+    displayName: z.string(),
+    avatar: z.string().url().optional(),
+    role: z.string().optional(),
+});
+
+export type ArticleAuthor = z.infer<typeof ArticleAuthor>;
+
+export const BaseArticleSchema = z.object({
+    type: ValidArticleTypes,
+    title: z.string(),
+    banner: z.string().url().optional(),
+    author: z.array(z.string()),
+    release: z.string().datetime().optional(),
+    video: z.string().url().optional(),
+});
+
+export type BaseArticle = z.infer<typeof BaseArticleSchema>;
+
+const ArticleSchema = BaseArticleSchema.merge(
+    z.object({
+        author: z.array(ArticleAuthor)
+    }
+));
+
+export type Article = z.infer<typeof ArticleSchema>;
 
 export const articleTypes: {
-    [k: string]: {
+    [key in ArticleTypes]: {
         color: TagColor,
         text: string
     } | undefined
