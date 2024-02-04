6e2a4ce6b46f161b05eccd00a79821e1291d21aa
gllherme <guicampos159@gmail.com>
2024-02-04 18:34:40 -0300

    refactor: creates AccountMenu props and default values

--- src/components/Header/AccountMenu/index.tsx
+++ src/components/Header/AccountMenu/index.tsx
@@ -2,17 +2,23 @@ import AvatarPlaceholder from "@/assets/Images/Avatar.jpg";
 import { Menu } from "lucide-react";
 import Image from "next/image";
 import styles from "./account-menu.module.css";
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
-const AccountMenu = () => {
-  const username = "Kadoodle";
+type Props = {
+  username?: string;
+  avatar?: StaticImport | string
+}
 
+const usernamePlaceholder = "Kadoodle"
+
+const AccountMenu: React.FC<Props> = ({ username = usernamePlaceholder, avatar = AvatarPlaceholder }) => {
   return ( 
     <section className={styles.container} >
       <Menu className={styles.icon} />
       <p className={styles.greeting}>
         Hey, <span>{username}</span>
       </p>
-      <Image className={styles.avatar} src={AvatarPlaceholder} alt="Avatar" />
+      <Image className={styles.avatar} src={avatar} alt="Avatar" />
     </section>
   );
 };
